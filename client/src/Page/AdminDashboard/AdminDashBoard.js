4f452dedf0854f833d70daa853aca91b326934aa
Amitrawat21 <rawatamit8285@gmail.com>
2024-08-21 08:41:29 +0530

    first commit

---
+++ client/src/Page/AdminDashboard/AdminDashBoard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './AdminDashBoard.css'
+import AdminDashboardcomponent from '../../components/AdminDashboardcomponent/AdminDashboardcomponent'
+const AdminDashBoard = () => {
+  return (
+    <div className='admin-dashboard'>
+    <AdminDashboardcomponent/>
+    </div>
+  )
+}
+
+export default AdminDashBoard
---
+++ client/src/components/AdminAssignTaskComponent/AdminAssignTaskComponent.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import './AdminAssignTaskComponen.css'
+import axios from "axios";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useSelector, useDispatch } from "react-redux";
+
+
+const AdminAssignTaskComponent = () => {
+    const { user } = useSelector((state) => state.auth);
+    const[data , setData] = useState([])
+
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+      };
+
+  const getData = async () => {
+    const response = await axios.get(`http://localhost:8000/user/Getadminassigntask/${user._id}`
+    );
+
+    setData(response.data.adminAssignedTasks)
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="AdminAssignTaskComponent">
+        <h2>assign Task</h2>
+        <div className="listproduct-format-mainkk">
+        <p>no</p>
+        <p>task</p>
+        <p>assignedAt</p>
+      </div>
+      <div className="listproduct-allproductsk">
+        <hr />
+
+        {data[0]?.tasks  &&
+          data[0]?.tasks .map((ele , index) => {
+            return (
+              <>
+                <div key={ele._id} className="listproduct-format-mainkk listproduct-formatk">
+                  <p>{index+1}</p>
+                  <p>{ele.task}</p>
+                  <p>{formatDate(ele.date)}</p>
+                </div>
+                <hr />
+              </>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default AdminAssignTaskComponent;
