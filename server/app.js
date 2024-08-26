79b53d05a6e52dc35cbb2361605fe11b1bdc57a2
Amitrawat21 <rawatamit8285@gmail.com>
2024-08-26 11:10:29 +0530

    sixteen  commilt

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["https://task-management-dun-omega.vercel.app"],
+  origin: ["http://localhost:3000"],
   credentials : true
 
 }));
